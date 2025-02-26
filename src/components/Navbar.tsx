5df80342234dde3ac07a3933bd25908b0960603f
achdaisy <oduordaizy@gmail.com>
2025-02-26 04:39:18 +0300

    Fix: Corrected the spelling for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar: React.FC = () => {   //React.FC just means react functional component
   return (
 
-    <nav className='navabar'>
+    <nav className='navbar'>
       <div className='logo'>Dashboard</div>
         <ul>
           <li>Home</li>
