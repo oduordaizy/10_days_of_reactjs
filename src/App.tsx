a0d80a78cf5a2212bfa4167b79a92625244fde0f
achdaisy <oduordaizy@gmail.com>
2025-02-26 04:36:03 +0300

    Update: Included the navbar and card component on the home oage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
 
 function App() {
  
   return (
     <>
-     React
+     <Navbar />
+
+     <div className='mainContent'>
+        <Card title='Section 1' content='Information about section 1'/>
+        <Card title='Section 2' content='Details about section 2'/>
+        <Card title='Section 3' content='Information about section 3'/>
+     </div>
+     
+     
     </>
   )
 }
