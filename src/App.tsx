968d431af65c5a609ba96d9e8568f151990b9107
achdaisy <oduordaizy@gmail.com>
2025-02-28 10:54:18 +0300

    Update: Called the TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css'
 import Card from './components/Card'
 import Navbar from './components/Navbar'
+import TodoList from './components/TodoList'
 
 function App() {
  
   return (
     <>
      <Navbar />
+    
+      <TodoList />
+     
+     
 
-     <div className='mainContent'>
-        <Card title='Section 1' content='Information about section 1'/>
-        <Card title='Section 2' content='Details about section 2'/>
-        <Card title='Section 3' content='Information about section 3'/>
-     </div>
      
      
     </>
