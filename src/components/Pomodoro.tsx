fbf70d002f449ea03756b4fcd4e325809a75d371
achdaisy <oduordaizy@gmail.com>
2025-03-03 07:14:34 +0300

    Updates: updated the pomodoro component and its styles

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -1,36 +1,62 @@
 import React, { useEffect, useState } from 'react'
 
-const Pomodoro:React.FC = () => {
-    const [time, setTime] = useState(25*60);
+
+const Pomodoro: React.FC = () => {
+  return (
+    <div className='pomodoro-container'>
+      <PomodoroFunction isBreak={false} />
+      <PomodoroFunction isBreak={true} />
+    </div>
+  )
+}
+
+interface PomodoroProps{
+  isBreak: boolean;
+}
+
+const PomodoroFunction :React.FC<PomodoroProps> = ({isBreak}) => {
+    const [time, setTime] = useState(isBreak ? 5*60 : 25*60);
     const [isRunning, setIsRunning] = useState(false);
-    const [isBreak, setIsBreak] = useState(false);
+    // const [isBreak, setIsBreak] = useState(false);
 
     //Runs whenever running changes
     //useEffect hook that depends on a varaible. Whenever is running updates, the effect runs
     useEffect(()=>{
-        let timer: NodeJS.Timeout;
+        let timer: number;
         if (isRunning){
-            timer = setInterval(()=>{
-                setTime((prev) = (prev > 0 ? prev - 1 : 0))
+            timer = window.setInterval(()=>{
+                setTime((prev:number) => (prev > 0 ? prev - 1 : 0))
             }, 1000)
             return () => clearInterval(timer);  //cleanup function.
         }
     }, [isRunning])
 
+    useEffect(() => {
+      if (time === 0) {
+        setIsRunning(false);
+      }
+    }, [time]);
+
+    const formatTime = (seconds: number) =>{
+      const minutes = Math.floor(seconds/60);
+      const sec = seconds % 60;
+      return `${minutes}: ${sec < 10 ? "0" : ""}${sec}`;
+    };
+
 
 
   return (
     <div className='pomodoro'>
-        <h2>{isBreak ? "Break Time" : "Work Time"}</h2>
-        <h1>{time}</h1>
+      {/* <h1>POMODORO TIMER</h1> */}
+        <h2>{isBreak ? "Break" : "Work"}</h2>
+        <p>{formatTime(time)}</p>
 
         <div className="buttons">
             <button onClick = {()=>setIsRunning(!isRunning)}>{isRunning ? "Pause": "Start"}</button>
             <button
             onClick={ () => {
             setIsRunning(false);
-            setTime(25*60);
-            setIsBreak(false)
+            setTime(isBreak ? 5 * 60 : 25 * 60);
             }}>
               Reset  
             </button>
