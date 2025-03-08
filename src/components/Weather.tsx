6e6303f549a2ccc8891fae5c0db4064fe0dcdb3a
achdaisy <oduordaizy@gmail.com>
2025-03-09 02:39:57 +0300

    feat: Weather Component

---
+++ src/components/Weather.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import { weatherData } from '../types/weatherTypes';
+
+interface WeatherProps{
+    initialCity?: string;
+}
+
+const Weather:React.FC<WeatherProps> = () => {
+    const [city, setCity] = useState<string>('');
+    const [weatherData, setWeatherData] = useState<weatherData | null> (null);
+    const [error, setError] = useState<string>('')
+
+    const fetchWeather = async() => {
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+            )
+            setWeatherData(response.data)
+            setError('')
+        }catch(err){
+            setError('City Not Found')
+            setWeatherData(null)
+        }
+
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        fetchWeather();
+    }
+
+  return (
+    <div className='weatherContainer'>
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Enter a City name' 
+                value={city}
+                onChange={(e)=>setCity(e.target.value)}
+            >
+            </input>
+            <button type='submit'>Get the Weather</button>
+        </form>
+
+        {error && <div className='error'>{error}</div>}
+
+        {weatherData && (
+            <div className="weatherInfo">
+                <h2>{weatherData.name}</h2>
+                <img
+                    src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                    alt={weatherData.weather[0].description}
+                />
+                <p>Temperature: {Math.round(weatherData.main.temp)}°C</p>
+                <p>Description: {weatherData.weather[0].description}</p>
+                
+            </div>
+        )}
+        
+        
+
+
+    </div>
+  )
+}
+
+export default Weather
