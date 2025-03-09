c1cb8fcc650fe239462967b2ff4ecebc3702329d
achdaisy <oduordaizy@gmail.com>
2025-03-09 16:30:13 +0300

    feat: Quotes Component

---
+++ src/components/Quote.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+
+// interface quoteProps {
+//   quote: string,
+//   author: string;
+// }
+
+const Quote:React.FC= () => {
+  const [quote, setQuote] = useState<string>('')
+  const [author, setAuthor] = useState<string>('')
+  const [error, setError] = useState<string>('')
+
+  const fetchQuote = async ()=>{
+    try{
+    const url = 'https://api.api-ninjas.com/v1/quotes';
+
+    const response = await fetch(url,{
+      method: "GET",
+      headers:{
+        'X-Api-Key': 'pYVFy76PZ8Uszja+34floQ==5QHrjbyKEE4tQzSR'
+      }
+    });
+    if (!response.ok) {
+      throw new Error("Failed to fetch quote");
+    } 
+    const result = await response.json()
+    if (result.length > 0){
+      setQuote(result[0].quote)
+      setAuthor(result[0].author)
+    }
+  } catch (err){
+      console.error("Error fetching quote:", err);
+      setError("Failed to load quote");
+
+  }
+  }
+
+  useEffect(()=>{
+    fetchQuote();
+
+    const interval = setInterval(()=>{
+      fetchQuote()
+    }, 24*60*60*1000)
+
+    return () => clearInterval(interval)
+    
+  }, [])
+
+  
+
+  return (
+    <div>
+      <h1>Daily Motivational Quote</h1>
+      {error ? (
+        <p style={{ color: 'red' }}>{error}</p>
+      ):(
+        <>
+          <h2>{quote}</h2>
+          <p>- {author}</p>
+        </>
+        
+      )}
+      
+    </div>
+  )
+}
+
+export default Quote
