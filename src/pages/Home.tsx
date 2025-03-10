e979d8c937e26d9125ecf32df1c8f4bcf2be986f
achdaisy <oduordaizy@gmail.com>
2025-03-11 00:57:02 +0300

    feat: Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Quote from '../components/Quote'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Quote/>
+
+    </div>
+  )
+}
+
+export default Home
